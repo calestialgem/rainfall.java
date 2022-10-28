692efd68908b23905a45a04ee6f37c912afac3a5
Cem GEÇGEL <gecgelcem@outlook.com>
2022-10-28 17:08:27 +0300

    fix: New Command Name

--- src/rainfall/launcher/Command.java
+++ src/rainfall/launcher/Command.java
@@ -5,7 +5,7 @@ import java.util.List;
 import rainfall.workspace.PhysicalName;
 
 public sealed interface Command {
-  record Create(PhysicalName created) implements Command {}
+  record New(PhysicalName created) implements Command {}
   record Check(List<PhysicalName> checked) implements Command {}
   record Test(List<PhysicalName> tested) implements Command {}
   record Build(PhysicalName built) implements Command {}
