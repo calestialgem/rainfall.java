766d0b7c8d33b9ea0487991d0450e52df2b015ca
Cem GEÇGEL <gecgelcem@outlook.com>
2022-10-30 12:04:04 +0300

    clean: Project

--- src/module-info.java
+++ src/module-info.java
@@ -1,10 +1,1 @@
-/**
- * Compiler for the Thrice programming language.
- *
- * @author calestialgem
- */
-module rainfall {
-  exports rainfall.utility;
-  exports rainfall.workspace;
-  exports rainfall.launcher;
-}
+module rainfall {}
