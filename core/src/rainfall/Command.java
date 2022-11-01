c2947a61faa6ac6a3a9f5fa8a506fa410e2aee13
Cem GEÇGEL <gecgelcem@outlook.com>
2022-11-02 00:04:46 +0300

    add: Commands

--- core/src/rainfall/Command.java
+++ core/src/rainfall/Command.java
@@ -1,5 +1,7 @@
 package rainfall;
 
+import java.util.List;
+
 public sealed abstract class Command {
   public static final class New extends Command {
     private final PhysicalName created;
@@ -8,5 +10,48 @@ public sealed abstract class Command {
     public PhysicalName created() { return created; }
   }
 
+  public static final class Check extends Command {
+    private final List<PhysicalName> checked;
+    private Check(List<PhysicalName> checked) { this.checked = checked; }
+
+    public List<PhysicalName> checked() { return checked; }
+  }
+
+  public static final class Test extends Command {
+    private final List<PhysicalName> tested;
+    private Test(List<PhysicalName> tested) { this.tested = tested; }
+
+    public List<PhysicalName> tested() { return tested; }
+  }
+
+  public static final class Build extends Command {
+    private final PhysicalName built;
+    private Build(PhysicalName built) { this.built = built; }
+
+    public PhysicalName built() { return built; }
+  }
+
+  public static final class Run extends Command {
+    private final PhysicalName run;
+    private final List<String> passed;
+    private Run(PhysicalName run, List<String> passed) {
+      this.run    = run;
+      this.passed = passed;
+    }
+
+    public PhysicalName run() { return run; }
+    public List<String> passed() { return passed; }
+  }
+
   public static New new_(PhysicalName created) { return new New(created); }
+  public static Check check(List<PhysicalName> checked) {
+    return new Check(checked);
+  }
+  public static Test test(List<PhysicalName> tested) {
+    return new Test(tested);
+  }
+  public static Build build(PhysicalName built) { return new Build(built); }
+  public static Run run(PhysicalName run, List<String> passed) {
+    return new Run(run, passed);
+  }
 }
--- core/src/rainfall/test/CommandTest.java
+++ core/src/rainfall/test/CommandTest.java
@@ -1,6 +1,9 @@
 package rainfall.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +14,42 @@ final class CommandTest {
   @Test void newHasName() {
     var created = PhysicalName.of("A").value();
     assertEquals(created, Command.new_(created).created());
+  }
+
+  @Test void checkHasEmptyChecked() {
+    assertTrue(Command.check(List.of()).checked().isEmpty());
+  }
+  @Test void checkHasChecked() {
+    var checked =
+      List.of(PhysicalName.of("A").value(), PhysicalName.of("B").value());
+    assertEquals(checked, Command.check(checked).checked());
+  }
+
+  @Test void testHasEmptyTested() {
+    assertTrue(Command.test(List.of()).tested().isEmpty());
+  }
+  @Test void testHasTested() {
+    var tested =
+      List.of(PhysicalName.of("A").value(), PhysicalName.of("B").value());
+    assertEquals(tested, Command.test(tested).tested());
+  }
+
+  @Test void buildHasName() {
+    var built = PhysicalName.of("A").value();
+    assertEquals(built, Command.build(built).built());
+  }
+
+  @Test void runHasName() {
+    var run = PhysicalName.of("A").value();
+    assertEquals(run, Command.run(run, List.of()).run());
+  }
+  @Test void runHasEmptyPassed() {
+    assertTrue(
+      Command.run(PhysicalName.of("A").value(), List.of()).passed().isEmpty());
+  }
+  @Test void runHasPassed() {
+    var passed = List.of("a", "b");
+    assertEquals(passed,
+      Command.run(PhysicalName.of("A").value(), passed).passed());
   }
 }
