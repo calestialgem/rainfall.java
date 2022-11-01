608ed8a4c6104b78b6474dfd6db12ccd855a5ee8
Cem GEÇGEL <gecgelcem@outlook.com>
2022-11-01 20:21:36 +0300

    refactor: Box Test Names

--- core/src/rainfall/test/BoxTest.java
+++ core/src/rainfall/test/BoxTest.java
@@ -7,15 +7,15 @@ import rainfall.Box;
 import static org.junit.jupiter.api.Assertions.*;
 
 final class BoxTest {
-  @Test void emptyBoxIsEmpty() { assertTrue(Box.empty().isEmpty()); }
-  @Test void emptyBoxIsNotFull() { assertFalse(Box.empty().isFull()); }
-  @Test void emptyBoxDoesNotHaveAValue() {
+  @Test void emptyIsEmpty() { assertTrue(Box.empty().isEmpty()); }
+  @Test void emptyIsNotFull() { assertFalse(Box.empty().isFull()); }
+  @Test void emptyDoesNotHaveValue() {
     assertThrows(UnsupportedOperationException.class, Box.empty()::value);
   }
 
-  @Test void fullBoxIsNotEmpty() { assertFalse(Box.full(1).isEmpty()); }
-  @Test void fullBoxIsFull() { assertTrue(Box.full(1).isFull()); }
-  @Test void fullBoxHasTheGivenValue() {
+  @Test void fullIsNotEmpty() { assertFalse(Box.full(1).isEmpty()); }
+  @Test void fullIsFull() { assertTrue(Box.full(1).isFull()); }
+  @Test void fullHasValue() {
     final var value = 1;
     assertEquals(value, Box.full(value).value());
   }
