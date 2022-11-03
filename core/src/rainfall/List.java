2da73076564fa31a856ecc2c5437aba0530b0f07
Cem GEÇGEL <gecgelcem@outlook.com>
2022-11-03 18:08:20 +0300

    add: List

---
+++ core/src/rainfall/List.java
@@ -0,0 +1,54 @@
+package rainfall;
+
+public sealed abstract class List<Element> {
+  @SafeVarargs public static <Element> List<Element> of(Element... elements) {
+    return new Array<>(elements);
+  }
+
+  public static <Element> Mutable<Element> mutable() { return new Mutable<>(); }
+
+  public abstract boolean isEmpty();
+  public abstract boolean isFinite();
+  public abstract int count();
+  public abstract Element get(int index);
+
+  private static final class Array<Element> extends List<Element> {
+    private final Element[] elements;
+    private Array(Element[] elements) { this.elements = elements; }
+
+    @Override public boolean isEmpty() { return elements.length == 0; }
+    @Override public boolean isFinite() { return elements.length > 0; }
+    @Override public int count() { return elements.length; }
+    @Override public Element get(int accessedIndex) {
+      return elements[accessedIndex];
+    }
+  }
+
+  public static final class Mutable<Element> extends List<Element> {
+    private Element[] elements;
+    private int       count;
+    @SuppressWarnings("unchecked") private Mutable() {
+      elements = (Element[]) new Object[0];
+      count    = 0;
+    }
+
+    @Override public boolean isEmpty() { return count == 0; }
+    @Override public boolean isFinite() { return count > 0; }
+    @Override public int count() { return count; }
+    @Override public Element get(int accessedIndex) {
+      return elements[accessedIndex];
+    }
+
+    @SuppressWarnings("unchecked") public void push(Element pushedElement) {
+      if (count == elements.length) {
+        var previousElements = elements;
+        elements = (Element[]) new Object[previousElements.length == 0 ? 1
+          : previousElements.length * 2];
+        System.arraycopy(previousElements, 0, elements, 0,
+          previousElements.length);
+      }
+      elements[count++] = pushedElement;
+    }
+    public void pop() { count--; }
+  }
+}
---
+++ core/src/rainfall/test/ListTest.java
@@ -0,0 +1,125 @@
+package rainfall.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import rainfall.List;
+
+final class ListTest {
+  @Test void emptyIsEmpty() { assertTrue(List.of().isEmpty()); }
+  @Test void emptyIsNotFinite() { assertFalse(List.of().isFinite()); }
+  @Test void emptyHasZeroCount() { assertEquals(0, List.of().count()); }
+
+  @Test void singleElementIsNotEmpty() { assertFalse(List.of(1).isEmpty()); }
+  @Test void singleElementIsFinite() { assertTrue(List.of(1).isFinite()); }
+  @Test void singleElementHasOneCount() { assertEquals(1, List.of(1).count()); }
+  @Test void singleElementHasElement() {
+    var element = 1;
+    assertEquals(element, List.of(element).get(0));
+  }
+
+  @Test void multiElementIsNotEmpty() { assertFalse(List.of(1, 2).isEmpty()); }
+  @Test void multiElementIsFinite() { assertTrue(List.of(1, 2).isFinite()); }
+  @Test void multiElementHasCorrectCount() {
+    assertEquals(2, List.of(1, 2).count());
+  }
+  @Test void multiElementHasElements() {
+    var elements = new int[] { 1, 2 };
+    var list     = List.of(elements[0], elements[1]);
+    assertEquals(elements[0], list.get(0));
+    assertEquals(elements[1], list.get(1));
+  }
+
+  @Test void newMutableIsEmpty() { assertTrue(List.mutable().isEmpty()); }
+  @Test void newMutableIsNotFinite() { assertFalse(List.mutable().isFinite()); }
+  @Test void newMutableHasZeroCount() {
+    assertEquals(0, List.mutable().count());
+  }
+
+  @Test void pushingToNewMutableMakesNotEmpty() {
+    var list = List.mutable();
+    list.push(1);
+    assertFalse(list.isEmpty());
+  }
+  @Test void pushingToNewMutableMakesFinite() {
+    var list = List.mutable();
+    list.push(1);
+    assertTrue(list.isFinite());
+  }
+  @Test void pushingToNewMutableIncrementsCount() {
+    var list = List.mutable();
+    list.push(1);
+    assertEquals(1, list.count());
+  }
+  @Test void pushingToNewMutableInsertsElement() {
+    var element = 1;
+    var list    = List.mutable();
+    list.push(element);
+    assertEquals(element, list.get(0));
+  }
+
+  @Test void pushingToFiniteDoesNotMakeEmpty() {
+    var list = List.mutable();
+    list.push(1);
+    list.push(1);
+    assertFalse(list.isEmpty());
+  }
+  @Test void pushingToFiniteKeepsFinite() {
+    var list = List.mutable();
+    list.push(1);
+    list.push(1);
+    assertTrue(list.isFinite());
+  }
+  @Test void pushingToFiniteIncrementsCount() {
+    var list = List.mutable();
+    list.push(1);
+    list.push(1);
+    assertEquals(2, list.count());
+  }
+  @Test void pushingToFiniteKeepsPreviousElement() {
+    var previousElement = 1;
+    var list            = List.mutable();
+    list.push(previousElement);
+    list.push(2);
+    assertEquals(previousElement, list.get(0));
+  }
+  @Test void pushingToFiniteKeepsPreviousElements() {
+    var previousElements = new int[] { 1, 2 };
+    var list             = List.mutable();
+    list.push(previousElements[0]);
+    list.push(previousElements[1]);
+    list.push(3);
+    assertEquals(previousElements[0], list.get(0));
+    assertEquals(previousElements[1], list.get(1));
+  }
+
+  @Test void poppingFromSingleElementMakesEmpty() {
+    var list = List.mutable();
+    list.push(1);
+    list.pop();
+    assertTrue(list.isEmpty());
+  }
+  @Test void poppingFromSingleElementMakesNotFinite() {
+    var list = List.mutable();
+    list.push(1);
+    list.pop();
+    assertFalse(list.isFinite());
+  }
+
+  @Test void poppingFromFiniteDecrementsCount() {
+    var list = List.mutable();
+    list.push(1);
+    list.pop();
+    assertEquals(0, list.count());
+  }
+  @Test void poppingCountManyFromFiniteMakesEmpty() {
+    var list = List.mutable();
+    list.push(1);
+    list.push(1);
+    for (var i = list.count(); i > 0; i--) { list.pop(); }
+    assertTrue(list.isEmpty());
+  }
+}
