5207c57118ea4c6bc022537fa7126e498962c021
Cem GEÇGEL <gecgelcem@outlook.com>
2022-11-03 18:19:38 +0300

    refactor: Message using Custom List

--- core/src/rainfall/Message.java
+++ core/src/rainfall/Message.java
@@ -5,31 +5,33 @@ public sealed abstract class Message {
     Message... causes) {
     return Result.failure(error(body, causes));
   }
+
   public static Message error(String body, Message... causes) {
-    return new Single("error", body, causes);
+    return new Single("error", body, List.of(causes));
   }
   public static Message warning(String body, Message... causes) {
-    return new Single("warning", body, causes);
+    return new Single("warning", body, List.of(causes));
   }
   public static Message info(String body, Message... causes) {
-    return new Single("info", body, causes);
+    return new Single("info", body, List.of(causes));
   }
+
   public static Message group() {
     throw new IllegalArgumentException("There are no messages to group!");
   }
   public static Message group(Message member) { return member; }
   public static Message group(Message... members) {
-    return new Multiple(members);
+    return new Multiple(List.of(members));
   }
 
   @Override public String toString() { return toString(0); }
   protected abstract String toString(int indentationLevel);
 
   private static final class Single extends Message {
-    private final String    title;
-    private final String    body;
-    private final Message[] causes;
-    private Single(String title, String body, Message... causes) {
+    private final String        title;
+    private final String        body;
+    private final List<Message> causes;
+    private Single(String title, String body, List<Message> causes) {
       this.title  = title;
       this.body   = body;
       this.causes = causes;
@@ -38,11 +40,11 @@ public sealed abstract class Message {
     @Override protected String toString(int indentationLevel) {
       var builder = new StringBuilder();
       builder.append(title).append(": ").append(body);
-      if (causes.length > 0) {
+      if (causes.count() > 0) {
         builder.append(" Due to:");
-        for (var cause : causes) {
+        for (var cause = 0; cause < causes.count(); cause++) {
           indent(indentationLevel + 1, builder);
-          builder.append(cause.toString(indentationLevel + 1));
+          builder.append(causes.get(cause).toString(indentationLevel + 1));
         }
       }
       return builder.toString();
@@ -50,15 +52,15 @@ public sealed abstract class Message {
   }
 
   private static final class Multiple extends Message {
-    private final Message[] members;
-    private Multiple(Message... members) { this.members = members; }
+    private final List<Message> members;
+    private Multiple(List<Message> members) { this.members = members; }
 
     @Override protected String toString(int indentationLevel) {
       var builder = new StringBuilder();
-      builder.append(members[0].toString(indentationLevel));
-      for (var member = 1; member < members.length; member++) {
+      builder.append(members.get(0).toString(indentationLevel));
+      for (var member = 1; member < members.count(); member++) {
         indent(indentationLevel, builder);
-        builder.append(members[member].toString(indentationLevel));
+        builder.append(members.get(member).toString(indentationLevel));
       }
       return builder.toString();
     }
