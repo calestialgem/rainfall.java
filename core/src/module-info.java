1a92f9538bded2632811077f567fd2bd80618e1c
Cem GEÇGEL <gecgelcem@outlook.com>
2022-11-12 11:25:05 +0300

    clean: Project

--- core/src/module-info.java
+++ core/src/module-info.java
@@ -1,11 +1,1 @@
-module rainfall {
-  exports rainfall.utility;
-  exports rainfall.physical;
-  exports rainfall.launcher;
-
-  requires org.junit.jupiter.api;
-  opens rainfall.qa.utility to org.junit.platform.commons;
-  opens rainfall.qa.physical to org.junit.platform.commons;
-  opens rainfall.qa.cli to org.junit.platform.commons;
-  opens rainfall.qa.launcher to org.junit.platform.commons;
-}
+module rainfall {}
