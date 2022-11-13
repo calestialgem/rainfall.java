cecc6438690a9a8467b259a8e85dcfbf7c4aa4aa
Cem GEÇGEL <gecgelcem@outlook.com>
2022-11-13 14:34:51 +0300

    rewrite: Optional

--- core/src/rainfall/utility/Optional.java
+++ core/src/rainfall/utility/Optional.java
@@ -1,40 +1,51 @@
 package rainfall.utility;
 
-public final class Optional<Value> {
-  private static final Optional EMPTY_INSTANCE = new Optional<>(null);
-
-  private final Value value;
-  private Optional(Value value) { this.value = value; }
-
-  public static <Value> Optional<Value> empty() { return EMPTY_INSTANCE; }
-  public static <Value> Optional<Value> full(Value value) {
-    return new Optional<>(value);
+public sealed abstract class Optional<Value> {
+  public static <Value> Optional<Value> present(Value value) {
+    return new Present<>(value);
   }
+  public static <Value> Optional<Value> absent() { return Absent.INSTANCE; }
 
-  public boolean isEmpty() { return value == null; }
-  public boolean isFull() { return value != null; }
-  public Value value() { return value; }
+  public abstract boolean isAbsent();
+  public abstract boolean isPresent();
+  public abstract Value value();
 
-  @Override public String toString() {
-    var buffer = new StringBuilder();
-    buffer.append('[');
-    if (isFull()) { buffer.append(value); }
-    buffer.append(']');
-    return buffer.toString();
-  }
-  @Override public int hashCode() {
-    var hash = 1;
-    if (isFull()) {
+  private static final class Present<Value> extends Optional<Value> {
+    private final Value value;
+    private Present(Value value) { this.value = value; }
+
+    @Override public boolean isAbsent() { return false; }
+    @Override public boolean isPresent() { return true; }
+    @Override public Value value() { return value; }
+
+    @Override public String toString() {
+      var buffer = new StringBuilder();
+      buffer.append('[');
+      buffer.append(value);
+      buffer.append(']');
+      return buffer.toString();
+    }
+    @Override public int hashCode() {
+      var hash = 1;
       hash *= 31;
       hash += value.hashCode();
+      return hash;
     }
-    return hash;
+    @Override public boolean equals(Object other) {
+      if (this == other) { return true; }
+      if (!(other instanceof Present asPresent)) { return false; }
+      return value.equals(asPresent.value);
+    }
   }
-  @Override public boolean equals(Object other) {
-    if (this == other) { return true; }
-    if (!(other instanceof Optional asOptional)) { return false; }
-    if (isEmpty()) { return asOptional.isEmpty(); }
-    if (asOptional.isEmpty()) { return false; }
-    return value.equals(asOptional.value);
+  private static final class Absent<Value> extends Optional<Value> {
+    private static final Absent INSTANCE = new Absent<>();
+
+    @Override public boolean isAbsent() { return true; }
+    @Override public boolean isPresent() { return false; }
+    @Override public Value value() { return null; }
+
+    @Override public String toString() { return "[]"; }
+    @Override public int hashCode() { return 31; }
+    @Override public boolean equals(Object other) { return this == other; }
   }
 }
